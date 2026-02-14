8026502f7040ea08161975f812eeb04f5c91be57
Harsh Kumar <harshkumar011045@gmail.com>
2026-02-14 19:28:01 +0530

    Final router and import fixes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import AboutSection from "./components/About";
+import ProductsSection from "./components/Productsection";
+import Footer from "./components/Footer";
+import AppRoutes from "./pages/routes/AppRoutes";
 
-import DigitalStandee from "../categories/DigitalStandee";
-import IndoorLED from "../categories/IndoorL";
-import LEDVideo from "../categories/LEDVideo";
-import OutdoorLED from "../categories/OutdoorLED";
-import WallMount from "../categories/WallMount";
-
-const AppRoutes = () => {
+function App() {
   return (
-    <Routes>
-      <Route path="/digital-standee" element={<DigitalStandee />} />
-      <Route path="/indoor-led" element={<IndoorLED />} />
-      <Route path="/led-video" element={<LEDVideo />} />
-      <Route path="/outdoor-led" element={<OutdoorLED />} />
-      <Route path="/wall-mount" element={<WallMount />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Hero />
+      <AboutSection />
+      <ProductsSection />
+      <AppRoutes />
+      <Footer />
+    </>
   );
-};
+}
 
-export default AppRoutes;
+export default App;
